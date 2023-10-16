22d1dc568cdf1dca863a2c24e2904ebcadd1ebff
h678137 <678137@stud.hvl.no>
2023-10-16 21:47:35 +0200

    Funker halveis

--- src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
+++ src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
@@ -1,11 +1,14 @@
 package no.hvl.dat100.prosjekt.kontroll;
 
+import java.util.Random;
+
 import no.hvl.dat100.prosjekt.TODO;
 import no.hvl.dat100.prosjekt.kontroll.dommer.Regler;
 import no.hvl.dat100.prosjekt.kontroll.spill.Handling;
 import no.hvl.dat100.prosjekt.kontroll.spill.HandlingsType;
 import no.hvl.dat100.prosjekt.kontroll.spill.Spillere;
 import no.hvl.dat100.prosjekt.modell.Kort;
+import no.hvl.dat100.prosjekt.modell.KortSamling;
 
 /**
  * Klasse som for å representere en vriåtter syd-spiller. Strategien er å lete
@@ -39,8 +42,51 @@ public class SydSpiller extends Spiller {
 
 		// TODO - START
 		/* first-fit strategi */
+		Kort [] hand = getHand().getAllekort();
+		KortSamling lov = new KortSamling();
+		KortSamling attere = new KortSamling();
+		
+//		sjekker og legger til hvilke kort vi kan spille
+		for(Kort k: hand) {
+			if(Regler.kanLeggeNed(k, topp)) {
+				if(Regler.atter(k)) {
+					attere.leggTil(k);
+				}
+				else {
+					lov.leggTil(k);
+				}
+			}
+		}
+		
+		Kort spill = null;
+		Kort[] spillFra = null;
+
+		if (!lov.erTom()) {
+			spillFra = lov.getAllekort();
+		} else if (!attere.erTom())  {
+			spillFra = attere.getAllekort();
+		}
+
+		Handling handling = null;
+		
+		if (spillFra != null) {
+			
+			Random r = new Random();
+			int p = r.nextInt(spillFra.length);
+			spill = spillFra[p];
+			handling = new Handling(HandlingsType.LEGGNED, spill);
+			// setAntallTrekk(0);
+			
+		} else if (getAntallTrekk() < Regler.maksTrekk()) {
+			handling = new Handling(HandlingsType.TREKK, null);
+		} else {
+			handling = new Handling(HandlingsType.FORBI, null);
+			// setAntallTrekk(0);
+		}
+
+		return handling;
+		
 	
-		throw new UnsupportedOperationException(TODO.method());
 	
 		// TODO - END
 	}
